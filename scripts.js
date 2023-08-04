7c8d10e14d8ae0737b7cb2c4151070c5972db3a0
Jairo Mercedes <jdavidmercedes@gmail.com>
2023-08-04 15:37:13 -0400

    corrigiendo tabs

--- scripts.js
+++ scripts.js
@@ -7,13 +7,15 @@ d.addEventListener('click', e => {
 
   if(target.matches('#compras_article_tabs a') || target.matches('#compras_article_tabs li')){
     if(target.matches('a')){
-      d.querySelectorAll('#compras_article_tabs li').forEach(li => li.classList.remove('active'))
+      d.querySelectorAll('#compras_article_tabs li a').forEach(li => li.classList.remove('active'))
       d.querySelectorAll('#compras_article iframe').forEach(li => li.classList.add('d-none'))
       
       const li = target.parentElement
       const id = li.id
       
-      // li.classList.add('active')
+      target.classList.add('active')
+
+      console.log(li)
       
       const iframe_id = id.replace('_li','_iframe')
       d.getElementById(iframe_id).classList.remove('d-none')
